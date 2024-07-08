b4bfd71d05c3c3617c67083af91f3007e3f542fc
Tove Rumar <tove@bitcraze.io>
2024-07-08 14:38:58 +0200

    Do not skip the first line and send every line by itself
    The buffering does not work well with our setup.
    The first line is needed to be flashed aswell, since it contains data and not metadata

--- flash36.py
+++ flash36.py
@@ -60,7 +60,7 @@ class PI():
         total = 0
         buf = ''
         buf_size = 0
-        for i in f[1:-1]:  # skip first and second lines
+        for i in f[:-1]:  # skip last line
             assert(i[0] == ':')            
             size = int(i[1:3],16)
             assert(size + 4 < 256)       
@@ -69,43 +69,42 @@ class PI():
                 addrl = int(i[5:7],16)
             assert(i[7:9] == '00')
             data = i[9:9 + size*2]
-            assert(len(data) == size*2)     
+            assert(len(data) == size*2)   
             buf += data
             buf_size += size
-		
-            if buf_size > 256 - 0x20 or i == f[-2]:
-                attempts = 0
-                num=0
-                while True:
-                    try:
-                        print (hex(addrh), hex(addrl), buf)
-                        crc = addrh + addrl
-                        for i in range(0,len(buf),2):
-                            val=buf[i]+buf[i+1]
-                            crc+=int(val,16)					
-                        assert(len(buf)/2 == buf_size)
-                        self.ser.write([0x3, buf_size + 4 + 1, buf_size, 0, addrh, addrl, crc & 0xff])
-                        for i in range(0,len(buf),2):
-                            val=buf[i]+buf[i+1]
-                            string = bytes.fromhex(val)
-                            self.ser.write(string)
-                        ret =self.ser.read(1)
-                        if ret == b'\x83':
-                            pass
-                        else:
-                            print ("error flash write returned ", hex(ret))
-                            raise RuntimeError('bad crc')
-                        self.check_written(buf,buf_size)
-                        break
-                    except Exception as e:
-                        attempts += 1
-                        self.conf()
-                        print (e)
-                        print ("attempts:",attempts)
-                total += buf_size
-                buf_size = 0
-                buf = ''
-                print ("Wrote %d bytes" % total)
+
+            attempts = 0
+            while True:
+                try:
+                    print (hex(addrh), hex(addrl), buf)
+                    crc = addrh + addrl
+                    for i in range(0,len(buf),2):
+                        val=buf[i]+buf[i+1]
+                        crc+=int(val,16)					
+                    assert(len(buf)/2 == buf_size)
+    
+                    self.ser.write([0x3, buf_size + 4 + 1, buf_size, 0, addrh, addrl, crc & 0xff])
+                    for i in range(0,len(buf),2):
+                        val=buf[i]+buf[i+1]
+                        string = bytes.fromhex(val)
+                        self.ser.write(string)
+                    ret =self.ser.read(1)
+                    if ret == b'\x83':
+                        pass
+                    else:
+                        print ("error flash write returned ", hex(ret))
+                        raise RuntimeError('bad crc')
+                    self.check_written(buf,buf_size)
+                    break
+                except Exception as e:
+                    attempts += 1
+                    self.conf()
+                    print (e)
+                    print ("attempts:",attempts)
+            total += buf_size
+            buf_size = 0
+            buf = ''
+            print ("Wrote %d bytes" % total)
 
         # reset device
         self.ser.write(b'\x02\x00')
